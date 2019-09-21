d663429b081b096e519663719aedd4d2426944ab
Emily Chatterjee <echatter@umich.edu>
2019-09-21 04:29:58 -0500

    got rid of commented out code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,38 +26,6 @@ function Home() {
   return <h2>Home</h2>;
 }
 
-//function Reg() {
-  //return <h2>Reg</h2>;
-//}
-
-// function Topic({ match }) {
-//   return <h3>Requested Param: {match.params.id}</h3>;
-// }
-
-// function Topics({ match }) {
-//   return (
-//     <div>
-//       <h2>Topics</h2>
-
-//       <ul>
-//         <li>
-//           <Link to={`${match.url}/components`}>Components</Link>
-//         </li>
-//         <li>
-//           <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-//         </li>
-//       </ul>
-
-//       <Route path={`${match.path}/:id`} component={Topic} />
-//       <Route
-//         exact
-//         path={match.path}
-//         render={() => <h3>Please select a topic.</h3>}
-//       />
-//     </div>
-//   );
-// }
-
 function Header() {
   return (
     <ul>
