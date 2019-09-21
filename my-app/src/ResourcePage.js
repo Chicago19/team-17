c3d4312ae7f42078603344fc8bad0e68694672c1
Kaveesha <weerasirikaveesha@gmail.com>
2019-09-21 00:46:27 -0500

    Started creating a resource page
    just has a basic <p> tag

---
+++ my-app/src/ResourcePage.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+
+export default class Exam extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      job:{}
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <p> Here are the resources:</p>
+    );
+  }
+}
