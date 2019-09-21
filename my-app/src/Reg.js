d36d45e9d5e80492d8ea69664327adb13cd49420
Emily Chatterjee <echatter@umich.edu>
2019-09-21 04:10:45 -0500

    done with base styling for reg form

--- my-app/src/Reg.js
+++ my-app/src/Reg.js
@@ -4,6 +4,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
 export default class Reg extends Component {
   constructor(props) {
@@ -57,105 +58,181 @@ export default class Reg extends Component {
             <FormControl />
           </InputGroup>
         </Col>
-        <br></br>
-      <text>Estado Civil:</text> <br></br>
-      <Form.Check type="radio" label="Single"/>
-      <input type="radio" name="Estado Civil" value="Casado/a"/> Married<br/>
-      <input type="radio" name="Estado Civil" value="Viduo/a"/> Widowed<br></br>
-      <input type="radio" name="Estado Civil" value="Divorciado/a"/> Divorced<br></br>
-      <input type="radio" name="Estado Civil" value=""/> Other<input type="text" name="Otro"/><br></br>
-    <br></br>
-      <text>Género:</text> <br></br>
-      <input type="radio" name="Gender" value="Male"/> Male<br/>
-      <input type="radio" name="Gender" value="Female"/> Female<br/><br/>
-      Domicilio: <input type="text" name="Address"/><br/>
-      Ciudad:<input type="text" name="City"/><br></br>
-      Estado:<input type="text" name="State"/><br></br>
-      Código Postal: <input type="text" name="Zipcode"/><br></br>
-
-      Numero de celular: <input type="text" name="Zipcode"/><br></br>
-      Numero de telefono de casa:<input type="text" name="lastName"/><br></br>
-      <br></br>
-      Nombre de Contacto de Emergencia:<input type="text" name="Emergency contact name"/><br></br>
-      Numero de celular: <input type="text" name="Emergency cell"/><br></br>
-      Parentesco:<input type="text" name="Relationship"/><br></br>
-
-      <br></br>
-        <text>¿Es Hispano o Latino?</text> <br></br>
-        <input type="radio" name="Es Hispano o Latino?" value="Yes"/> Male<br/>
-        <input type="radio" name="Es Hispano o Latino?" value="No"/> Female<br/>
-      <br></br>
-        País De Origen: <input type="text" name="Country of Origin"/>
-      <br/><br/>
-        <text>Are you from one or more of the following racial groups?</text> <br></br>
-        <input type="checkbox" name="Race" value="American Indian or Alaska Native"/> American Indian or Alaska Native<br/>
-        <input type="checkbox" name="Race" value="Asian"/> Asian<br/>
-        <input type="checkbox" name="Race" value="Black or African American"/> Black or African American<br></br>
-        <input type="checkbox" name="Race" value="Native Hawaiian or Pacific Islander"/> Native Hawaiian or Pacific Islander<br></br>
-        <input type="checkbox" name="Race" value="White"/> Divorced<br></br>
-        <input type="checkbox" name="Race" value="Hispanic or Latino"/> Hispanic<br></br>
-      <br></br>
-        <text>Identify your primary ethnic group:</text> <br></br>
-        <input type="radio" name="Race" value="American Indian or Alaska Native"/> American Indian or Alaska Native<br/>
-        <input type="radio" name="Race" value="Asian"/> Asian<br/>
-        <input type="radio" name="Race" value="Black or African American"/> Black or African American<br></br>
-        <input type="radio" name="Race" value="Native Hawaiian or Pacific Islander"/> Native Hawaiian or Pacific Islander<br></br>
-        <input type="radio" name="Race" value="White"/> Divorced<br></br>
-        <input type="radio" name="Race" value="Hispanic or Latino"/> Hispanic<br></br>
-      <br></br>
-        <text>Es inglés su segundo idioma?:</text> <br></br>
-        <input type="radio" name="Second Language" value="Yes"/> Yes<br/>
-        <input type="radio" name="Second Language" value="No"/> No<br/>
-        ¿Cuál es tu primer idioma?: <input type="text" name="Native Language"/>
-      <br/><br/>
-        Tiempo pasado en los Estados Unidos: <input type="text" name="Time in the USA"/>
-      <br/><br/>
-        Cómo se enteró acerca de PODER? <br/>
-        <input type="radio" name="referral" value="friendfam"/> Amigo o Familiar <br/>
-        <input type="radio" name="referral" value="community"/> Organización Comunitaria <br/>
-        <input type="radio" name="referral" value="church"/> Iglesia <br/>
-        <input type="radio" name="referral" value="other"/> Otro <br/>
-        <input type="radio" name="referral" value="volunteer"/> Voluntario <br/>
-        <input type="radio" name="referral" value="internet"/> Internet <br/>
-        <input type="radio" name="referral" value="pamphlet"/> Folleto <br/>
         <br/>
-        Cual es la razón principal que usted tuvo para eligir PODER? <br/>
-        <input type="radio" name="choose" value="english"/> Aprender Ingles <br/>
-        <input type="radio" name="choose" value="job"/> Obtener un trabajo <br/>
-        <input type="radio" name="choose" value="training"/> Entrenamiento/asistencia de empleo <br/>
-        <input type="radio" name="choose" value="computer"/> Computadoras/tecnologia <br/>
-        <input type="radio" name="choose" value="other"/> Otro <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>Estado Civil</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Soltero/a"/>
+              <Form.Check type="radio" label="Casado/a"/>
+              <Form.Check type="radio" label="Viduo/a"/>
+              <Form.Check type="radio" label="Divorciado/a"/>
+              <input type="radio" name="Estado Civil" value=""/> Other <input type="text" name="Otro"/>
+            </Card.Body>
+          </Card>
+        </Col>
         <br/>
-        Por favor seleccione el sitio del programa de PODER donde desea recibir las clases: <br/>
-        <input type="radio" name="location" value="pulaski"/> Pulaski/Marquette Bank (6155 S. Pulaski) <br/>
-        <input type="radio" name="location" value="kedzie"/> Kedzie/MFS (6422 S. Kedzie) <br/>
-        <input type="radio" name="location" value="sawyer"/> Sawyer/St. Gall Parish (5533 S. Sawyer) <br/>
-        <input type="radio" name="location" value="kostner"/> Stevenson/MFS (8010 S. Kostner) <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>Género</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Masculino"/>
+              <Form.Check type="radio" label="Femenino"/>
+            </Card.Body>
+          </Card>
+        </Col>
         <br/>
-        Por favor seccione el tiempo de clase preferida: <br/>
-        <input type="radio" name="time" value="morning"/> Manana
-        <input type="radio" name="time" value="night"/> Noches <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>Información de Contacto</Card.Body>
+            <Card.Body>
+              Domicilio: <FormControl/><br/>
+              Ciudad: <FormControl/><br/>
+              Estado: <FormControl/><br/>
+              Código Postal: <FormControl/><br/>
+              Número de celular:<FormControl/><br/>
+              Número de telefono de casa: <FormControl/><br/>
+              Nombre de Contacto de Emergencia: <FormControl/><br/>
+              Número de celular: <FormControl/><br/>
+              Parentesco: <FormControl/><br/>
+            </Card.Body>
+          </Card>
+        </Col>
         <br/>
-        Cual es su principal medio de transporte? <br/>
-        <input type="radio" name="transportation" value="walk"/> Caminar
-        <input type="radio" name="transportation" value="bus"/> Bus <br/>
-        <input type="radio" name="transportation" value="car"/> Carro
-        <input type="radio" name="transportation" value="train"/> Tren <br/>
-        <input type="radio" name="transportation" value="driven"/> Maneja un amigo o familiar <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>¿Es Hispano o Latino?</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Si"/>
+              <Form.Check type="radio" label="No"/>
+            </Card.Body>
+          </Card>
+        </Col>
         <br/>
-        Para uso de Oficina Solamente: <br/>
-        Registration Date: <input type="text" name="date"/> <br/>
-        Keyword: <input type="text" name="keyword"/> <br/>
-        Placement exam score: <br/>
-        Oral: <input type="text" name="oral"/> <br/>
-        Written: <input type="text" name="written"/> <br/>
-        Level: <input type="text" name="level"/> <br/>
-        Testing date: <input type="text" name="testdate"/> <br/>
-        BEST exam: <input type="text" name="best"/>
-        ICCB intake form: <input type="text" name="iccb"/>
-        Empowerment survey: <input type="text" name="empowerment"/>
-
-      <button style={{height:'100px', width:'200px', fontSize:'20px'}}> <a href = "/Exam">Go to Exam</a></button>
+        <Col sm={8}>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>País De Origen</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl />
+          </InputGroup>
+        </Col>
+        <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>Are you from one or more of the following racial groups?</Card.Body>
+            <Card.Body>
+              <Form.Check type="checkbox" label="American Indian or Alaska Native"/>
+              <Form.Check type="checkbox" label="Asian"/>
+              <Form.Check type="checkbox" label="Black or African American"/>
+              <Form.Check type="checkbox" label="Native Hawaiian or Pacific Islander"/>
+              <Form.Check type="checkbox" label="White"/>
+              <Form.Check type="checkbox" label="Hispanic or Latino"/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>¿Es inglés su segundo idioma?</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Si"/>
+              <Form.Check type="radio" label="No"/>
+              ¿Cuál es tu primer idioma? <input type="text" name="Native Language"/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <br/>
+        <Col sm={8}>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>Tiempo pasado en los Estados Unidos</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl />
+          </InputGroup>
+        </Col>
+        <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>¿Cómo se enteró acerca de PODER?</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Amigo o Familiar"/>
+              <Form.Check type="radio" label="Organización Comunitaria"/>
+              <Form.Check type="radio" label="Iglesia"/>
+              <Form.Check type="radio" label="Otro"/>
+              <Form.Check type="radio" label="Voluntario"/>
+              <Form.Check type="radio" label="Internet"/>
+              <Form.Check type="radio" label="Folleto"/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>¿Cual es la razón principal que usted tuvo para eligir PODER?</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Aprender Ingles"/>
+              <Form.Check type="radio" label="Obtener un trabajo"/>
+              <Form.Check type="radio" label="Entrenamiento/asistencia de empleo"/>
+              <Form.Check type="radio" label="Computadoras/tecnologia"/>
+              <Form.Check type="radio" label="Otro"/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>Por favor seleccione el sitio del programa de PODER donde desea recibir las clases</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Pulaski/Marquette Bank (6155 S. Pulaski)"/>
+              <Form.Check type="radio" label="Kedzie/MFS (6422 S. Kedzie)"/>
+              <Form.Check type="radio" label="Sawyer/St. Gall Parish (5533 S. Sawyer)"/>
+              <Form.Check type="radio" label="Stevenson/MFS (8010 S. Kostner)"/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>Por favor seccione el tiempo de clase preferida</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Mañana"/>
+              <Form.Check type="radio" label="Noches"/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>¿Cual es su principal medio de transporte?</Card.Body>
+            <Card.Body>
+              <Form.Check type="radio" label="Caminar"/>
+              <Form.Check type="radio" label="Bus"/>
+              <Form.Check type="radio" label="Carro"/>
+              <Form.Check type="radio" label="Tren"/>
+              <Form.Check type="radio" label="Maneja un amigo o familiar"/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <br/>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>Para uso de Oficina Solamente</Card.Body>
+            <Card.Body>
+              Registration Date: <FormControl/><br/>
+              Keyword: <FormControl/><br/>
+              Oral exam score: <FormControl/><br/>
+              Written exam score: <FormControl/><br/>
+              Level: <FormControl/><br/>
+              Testing date: <FormControl/><br/>
+              BEST exam: <FormControl/><br/>
+              ICCB intake form: <FormControl/><br/>
+              Empowerment survey: <FormControl/><br/>
+            </Card.Body>
+          </Card>
+        </Col>
+        <br/>
+        <Button variant="outline-dark"><a href="/Exam">Submit</a></Button>
       </form>
     );
   }
